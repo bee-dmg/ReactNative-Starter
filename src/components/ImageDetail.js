1feca5d533d03ae3d644afbc234a5eec60bbb3ef
bee-dmg <brandonschmersal@gmail.com>
2022-09-05 10:48:57 -0400

    props.

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
-const ImageDetail = ({text}) => {
-    return <Text>Image {text}</Text>
+const ImageDetail = (props) => {
+    return <View>
+        <Image source={props.imageSource} />
+            <Text>
+
+                {props.title}
+
+            </Text>
+        </View>
 };
 
 const styles = StyleSheet.create({});
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import ImageDetail from '../components/ImageDetail';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import ImageDetail from "../components/ImageDetail";
 
 const ImageScreen = () => {
-    return <View>
-        <ImageDetail text="image1"/>
-        <ImageDetail/>
-        <ImageDetail/>
-        <ImageDetail/>
-        <ImageDetail/>
-        </View>
+  return (
+    <View>
+      <ImageDetail
+        title="Forest"
+        imageSource={require("../../assets/forest.jpg")}
+      />
+      <ImageDetail
+        title="Beach"
+        imageSource={require("../../assets/beach.jpg")}
+      />
+      <ImageDetail
+        title="Mountain"
+        imageSource={require("../../assets/mountain.jpg")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
