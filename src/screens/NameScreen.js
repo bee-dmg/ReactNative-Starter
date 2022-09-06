baf05064bf69ead03a3ed631b98385e47fa77799
bee-dmg <brandonschmersal@gmail.com>
2022-09-06 08:43:17 -0400

    Password validation.

--- src/screens/NameScreen.js
+++ src/screens/NameScreen.js
@@ -1,20 +1,30 @@
-import React, {useState} from 'react';
-import {View, StyleSheet, Text, TextInput} from 'react-native';
+import React, { useState } from "react";
+import { View, StyleSheet, Text, TextInput } from "react-native";
 
 const TextScreen = () => {
+  const [input, setInput] = useState("");
 
-    const [input, setInput] = useState("");
-
-    return <View><TextInput style={styles.input} autoCapitalize="none" autoCorrect={false} />
-    <Text>{input}</Text></View>
+  return (
+    <View>
+        <Text>Enter Password: </Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={input}
+        onChangeText={(newValue) => setInput(newValue)}
+      />
+      {input.length>=5?null:<Text>Password must be more than 5 characters</Text>}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    input: {
-        margin: 15,
-        borderColor: 'black',
-        borderWidth:1
-    }
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1,
+  },
 });
 
 export default TextScreen;
