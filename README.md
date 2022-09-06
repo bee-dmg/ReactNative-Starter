b5e1a516bae6bfddc9905fa07dd34b5999132524
bee-dmg <brandonschmersal@gmail.com>
2022-09-06 08:30:16 -0400

    Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Starts with expo start ran in main directory.
+
+This is an app that explores some functionality of react native.
--- src/screens/NameScreen.js
+++ src/screens/NameScreen.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Text, TextInput} from 'react-native';
 
 const TextScreen = () => {
-    return <View><Text>Hello</Text></View>
+
+    const [input, setInput] = useState("");
+
+    return <View><TextInput style={styles.input} autoCapitalize="none" autoCorrect={false} />
+    <Text>{input}</Text></View>
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth:1
+    }
+});
 
 export default TextScreen;
